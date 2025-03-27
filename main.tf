6362bdb56ae54b2cc325597f93c8b36c9501ae78
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-03-27 12:04:34 +0530

    Creating main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,19 @@ module "fdo-aks" {
   source  = "app.terraform.io/hashicorp-support-eng/fdo-aks/azurerm"
   version = "0.0.1"
   # insert required variables here
+  appId = var.appId
+  hostname = var.hostname
+  password = var.password
+}
+variable "appId"{
+type = string
+description = "Application ID"
+}
+variable "hostname"{
+type = string
+description = "Hostname for app"
+}
+variable "password"{
+type = string
+description = "password"
 }
